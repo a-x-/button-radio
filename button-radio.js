097e8a0c6afcc8de30a82565626366992f73c59f
Inventorem <me@invntrm.ru>
2014-11-06 16:47:54 +0300

    Add events

--- button-radio.js
+++ button-radio.js
@@ -19,13 +19,17 @@ var ButtonRadio = (function () {
         this.options.change = function () {
         };
     }
-    // options.change = options.change || function(){};
-    // options.change = options.change || function(){};
-    // options.change = options.change || function(){};
-    [].slice.call(this.group.querySelectorAll('button')).forEach(function (button) {
+
+    //
+    // Add listeners for buttons' click events
+    [].slice.call(this.group.querySelectorAll('button:not(.no-radio)')).forEach(function (button) {
         button.addEventListener('click', function (event) {
             var prevButton = this.getMode() && this.checkedButton;
             if (this.getMode() === button.dataset.value) { // if current button equal a previous button
+                try {
+                    this.checkedButton.blur();
+                } catch (e) {
+                }
                 this.checkedButton = undefined;
             }
             else {
@@ -33,13 +37,43 @@ var ButtonRadio = (function () {
                 this.checkedButton = button;
             }
             prevButton && prevButton.classList.remove('checked');
-            this.options.change({
+            var response = {
                 prevButton: prevButton,
                 curButton: this.checkedButton,
                 groupNode: this.group,
                 value: this.checkedButton && this.checkedButton.dataset.value,
                 prevValue: prevButton && prevButton.dataset.value
-            }); // call back
+            };
+            //
+            // Trigger event tool
+            var triggerEvent = function (name, data) {
+                this.group.dispatchEvent(new CustomEvent(name, {detail: {response: response, data: data}}));
+            }.bind(this);
+
+            //
+            // Trigger events and callbacks
+            //
+
+            //
+            // Trigger event: any general
+            triggerEvent('state-changed');
+            this.options.change(response); // call back, also
+            //
+            // Trigger event: any active/passive
+            if (!response.prevButton) triggerEvent('radio-enter');
+            if (!response.curButton) triggerEvent('radio-out');
+            //
+            // Trigger event: the mode is active
+            if (response.value){
+                triggerEvent('state-in-' + response.value);
+                triggerEvent('change-state-' + (response.value), {state: 'in'});
+            }
+            //
+            // Trigger event: the previous mode is passive
+            if (response.prevValue) {
+                triggerEvent('state-out-' + response.prevValue);
+                triggerEvent('change-state-' + (response.prevValue), {state: 'out'});
+            }
         }.bind(this));
     }.bind(this));
 });
